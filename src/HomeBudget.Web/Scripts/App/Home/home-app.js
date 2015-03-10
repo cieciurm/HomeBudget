7fa08655274417a3d22a29c2fb1b942922265999
Jaroslaw Adamczyk <jaradam1992@gmail.com>
2015-03-10 20:55:21 +0100

    Add AngularJS 1.3

---
+++ src/HomeBudget.Web/Scripts/App/Home/home-app.js
@@ -0,0 +1,8 @@
+﻿(function (window, angular, undefined) {
+    var app = angular.module("homeApp", []);
+
+    app.controller("homeController", ["$scope", function($scope) {
+        $scope.Home = "test";
+    }]);
+
+})(window, angular, undefined);
