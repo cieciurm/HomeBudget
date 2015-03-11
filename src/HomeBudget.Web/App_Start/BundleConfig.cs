cdebfffbe11761a183749b4f3471218d49caf26d
Jaroslaw Adamczyk <jaradam1992@gmail.com>
2015-03-11 22:29:10 +0100

    Add bundle os SlickGrid

--- src/HomeBudget.Web/App_Start/BundleConfig.cs
+++ src/HomeBudget.Web/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace HomeBudget.Web
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.event.drag.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -17,6 +18,11 @@ namespace HomeBudget.Web
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
                         "~/Scripts/angular.js",
                         "~/Scripts/angular-resource.js"));
+
+
+            bundles.Add(new ScriptBundle("~/bundles/slickgrid")
+                .IncludeDirectory("~/Scripts/SlickGrid", "*.js", true));
+
 
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
